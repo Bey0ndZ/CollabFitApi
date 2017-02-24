dbddea5ee7e07dc050a2d285257e94d1130903ad
sumanthk <sumanth.kulkarni@citrix.com>
2017-02-23 16:46:40 -0800

    init

---
+++ src/main/java/com/collabfit/CollabFitApiApplication.java
@@ -0,0 +1,12 @@
+package com.collabfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabFitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabFitApiApplication.class, args);
+	}
+}
